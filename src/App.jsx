3372b432da0f3c3a6906d40587c15adad0ff6d62
FlameWolf <ajifocus@LIVE.COM>
2021-12-09 14:36:20 +0530

    Initial file upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Alert, Modal, ModalBody, ModalFooter, ModalHeader, ModalTitle } from "solid-bootstrap";
+import { createSignal } from "solid-js";
+
+function App() {
+	const [showModal, setShowModal] = createSignal(false);
+
+	return (
+		<>
+			<div className="row my-2">
+				<div className="d-flex justify-content-center">
+					<h1>Hello, World!</h1>
+				</div>
+			</div>
+			<Alert dismissible="true">This is a simple alert.</Alert>
+			<Modal>
+				<ModalHeader>
+					<ModalTitle>This is the modal title</ModalTitle>
+				</ModalHeader>
+				<ModalBody>
+					<p>This is the modal body.</p>
+				</ModalBody>
+				<ModalFooter>
+					<p>This is the modal footer.</p>
+				</ModalFooter>
+			</Modal>
+		</>
+	);
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import { render } from "solid-js/web";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+
+render(App, document.getElementById("root"));
