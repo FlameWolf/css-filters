baa04ea059f09205c099b2f9fdd9b36e861d2556
FlameWolf <ajifocus@LIVE.COM>
2022-01-14 16:29:44 +0530

    Added code to make CSS filter playground

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,48 @@
-import { Alert, Modal, ModalBody, ModalFooter, ModalHeader, ModalTitle } from "solid-bootstrap";
-import { createSignal } from "solid-js";
+import { createMemo, For } from "solid-js";
+import RangeSlider from "./components/RangeSlider";
+import { filterStore } from "./store/filter-store";
 
 function App() {
-	const [showModal, setShowModal] = createSignal(false);
+	const filterString = createMemo(() => {
+		return filterStore.filters.reduce((result, filter) => {
+			const filterName = filter.name;
+			const defaultValue = filterStore.filterDefaultValue(filterName);
+			const currentValue = filterStore.filterCurrentValue(filterName);
+			const transform = filter.transform || (x => x);
+			return defaultValue === currentValue ?
+				result :
+				`${result}${filterName}: ${transform(currentValue)}; `;
+		}, "").trim();
+	});
 
 	return (
 		<>
-			<div className="row my-2">
-				<div className="d-flex justify-content-center">
-					<h1>Hello, World!</h1>
+			<div class="row">
+				<h4 class="mb-4 fw-bold">CSS Filter Playground</h4>
+			</div>
+			<div class="row">
+				<div class="col-10">
+					<div class="position-relative">
+						<img class="img-fluid" src="/src/assets/images/scenery.jpg" style={filterString()}/>
+						<div class="position-absolute top-0 start-0 w-100 mt-4 px-2">
+							<div class="d-flex align-items-center mb-2">
+								<div class="highlighted">CSS:</div>
+								<button class="btn btn-primary border border-white ms-auto" onClick={() => filterStore.resetAllFilterValues()}>Reset Image</button>
+							</div>
+							<textarea class="form-control" value={filterString()}></textarea>
+						</div>
+					</div>
+				</div>
+				<div class="col-2">
+					<div class="d-flex flex-column justify-content-end">
+						<For each={filterStore.filters}>
+						{
+							(filter, index) => <RangeSlider {...filter}/>
+						}
+						</For>
+					</div>
 				</div>
 			</div>
-			<Alert dismissible="true">This is a simple alert.</Alert>
-			<Modal show={showModal()}>
-				<ModalHeader onHide={() => setShowModal(value => !value)}>
-					<ModalTitle>This is the modal title</ModalTitle>
-				</ModalHeader>
-				<ModalBody>
-					<p>This is the modal body.</p>
-				</ModalBody>
-				<ModalFooter>
-					<p>This is the modal footer.</p>
-				</ModalFooter>
-			</Modal>
 		</>
 	);
 }
---
+++ src/store/filter-store.js
@@ -0,0 +1,57 @@
+import { createStore } from "solid-js/store";
+
+export const [filterStore, setFilterStore] = createStore({
+	filters: [
+		{ name: "brightness", min: 0, max: 200, unit: "%" },
+		{ name: "contrast", min: 0, max: 200, unit: "%" },
+		{ name: "saturate", min: 0, max: 100, unit: "%", text: "Saturation" },
+		{ name: "drop-shadow", min: 0, max: 50, default: 50, unit: "px", transform: input => `0 0 ${input} #000000`, text: "Drop Shadow" },
+		{ name: "grayscale", min: 0, max: 100, default: 0, unit: "%", text: "Greyscale" },
+		{ name: "sepia", min: 0, max: 100, default: 0, unit: "%" },
+		{ name: "invert", min: 0, max: 100, default: 0, unit: "%", text: "Inversion" },
+		{ name: "opacity", min: 0, max: 100, default: 100, unit: "%" },
+		{ name: "hue-rotate", min: 0, max: 360, default: 0, unit: "deg", text: "Hue Rotation" },
+		{ name: "blur", min: 0, max: 50, default: 0, unit: "px", text: "Blurriness" }
+	],
+	filterDisplayText: name => {
+		const filter = filterStore.filters.find(filter => filter.name === name);
+		return filter.text || `${name[0].toUpperCase()}${name.slice(1)}`;
+	},
+	updateFilterValue: (name, value) => {
+		setFilterStore(
+			"filters",
+			filter => filter.name === name,
+			filter => ({
+				value
+			})
+		);
+	},
+	filterDefaultValue: name => {
+		const filter = filterStore.filters.find(filter => filter.name === name);
+		return filter.default || filter.max < filter.min ?
+			filter.min :
+			(filter.min + (filter.max - filter.min) / 2);
+	},
+	filterCurrentValue: name => {
+		const filter = filterStore.filters.find(filter => filter.name === name);
+		return filter.value !== undefined ? filter.value : filterStore.filterDefaultValue(name);
+	},
+	resetFilterValue: name => {
+		setFilterStore(
+			"filters",
+			filter => filter.name === name,
+			filter => ({
+				value: undefined
+			})
+		);
+	},
+	resetAllFilterValues: () => {
+		setFilterStore(
+			"filters",
+			{},
+			filter => ({
+				value: undefined
+			})
+		);
+	}
+});
