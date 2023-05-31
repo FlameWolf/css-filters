76c66391354d00a45e8b9d17a8cd4f724379b610
FlameWolf <agnijith@gmail.com>
2023-05-31 16:50:30 +0530

    Add increment/decrement buttons for slider

--- src/components/RangeSlider.jsx
+++ src/components/RangeSlider.jsx
@@ -1,14 +1,32 @@
 import { filterStore } from "../store/filter-store";
 
 export default props => {
+	let rangeInput;
+
+	const updateFilterValue = value => {
+		if(value < props.min || value > props.max) {
+			return;
+		}
+		filterStore.updateFilterValue(props.name, value);
+	};
+
+	const decrementFilterValue = () => updateFilterValue(parseFloat(rangeInput.value) - 1);
+	const incrementFilterValue = () => updateFilterValue(parseFloat(rangeInput.value) + 1);
+
 	return (
 		<div class="my-1 col-sm-6 col-md-4 col-lg-12">
 			<div class="d-flex">
 				<label htmlFor={props.name}>{filterStore.filterDisplayText(props.name)}</label>
 				<span class="ms-auto">{filterStore.filterCurrentValue(props.name)}{props.unit}</span>
 			</div>
-			<div class="d-flex">
-				<input id={props.name} type="range" min={props.min} max={props.max} value={filterStore.filterCurrentValue(props.name)} onInput={event => filterStore.updateFilterValue(props.name, event.target.value)}/>
+			<div class="d-flex align-items-center">
+				<button class="btn btn-lg p-0" onPress={decrementFilterValue} onClick={decrementFilterValue}>
+					<i class="bi bi-dash-circle"></i>
+				</button>
+				<input ref={rangeInput} id={props.name} class="form-range px-1" type="range" min={props.min} max={props.max} value={filterStore.filterCurrentValue(props.name)} onInput={() => updateFilterValue(parseFloat(rangeInput.value))}/>
+				<button class="btn btn-lg p-0" onPress={incrementFilterValue} onClick={incrementFilterValue}>
+					<i class="bi bi-plus-circle"></i>
+				</button>
 				<button class="btn btn-sm btn-secondary ms-2" onClick={() => filterStore.resetFilterValue(props.name)}>Reset</button>
 			</div>
 		</div>
--- src/library.jsx
+++ src/library.jsx
@@ -1,5 +1,5 @@
 export const trimFileName = function (fileName) {
-	if(fileName.length <= 12) {
+	if (fileName.length <= 12) {
 		return fileName;
 	}
 	const lastPeriodIndex = fileName.lastIndexOf(".");
@@ -8,3 +8,33 @@ export const trimFileName = function (fileName) {
 	}
 	return `${fileName.substr(0, 3)}${String.fromCharCode(0x2026)}${fileName.substr(lastPeriodIndex - 3)}`;
 };
+
+(function () {
+	let isPressed = false;
+	let pressTimer = false;
+	const pressedEvent = new CustomEvent("press", {
+		bubbles: true,
+		cancelable: true,
+		composed: true
+	});
+	const releasedEvent = new CustomEvent("release", {
+		bubbles: true,
+		cancelable: true,
+		composed: true
+	});
+	const pressed = event => {
+		if (event.buttons === 1) {
+			isPressed = true;
+			pressTimer = setInterval(() => {
+				event.target.dispatchEvent(pressedEvent);
+			}, 100);
+		}
+	};
+	const released = event => {
+		isPressed = false;
+		clearInterval(pressTimer);
+		event.target.dispatchEvent(releasedEvent);
+	};
+	globalThis.addEventListener("mousedown", pressed);
+	globalThis.addEventListener("mouseup", released);
+})();
